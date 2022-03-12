c1d1e07741f4f5385eaed7440f6f4bfa27ee7214
Caroline <lallcaroline600@gmail.com>
2022-03-12 19:10:01 +0530

    make it better

--- src/component/todoreact/todo.js
+++ src/component/todoreact/todo.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import "./style.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Todo = () => {
   //getting localStorage data
@@ -74,20 +75,27 @@ const Todo = () => {
  
   return ( 
   <>
-   <div className="main-div">
+  <div className="container my-5 pt-5 main-head">
+  <h1 className="display-5 fw-bold">Todo List</h1>
+  <div className="col-lg-6 mx-auto">
+      <p className="mb-5 todo-para">Manage your daily chores efficiently by adding your daily tasks</p>
+    </div>
+  </div>
+
+
+   <div className="main-div mt-5">
       <div className="child-div">
-       <figure>
-         <img src="./images/todo.png" alt="todo-logo" />
-         <figcaption>Add your List Here</figcaption>
-       </figure>
+         <img src="./images/to-tasks.svg" alt="todo-logo" />
+         <h3 className="main-heading"><strong>Add your List Here</strong></h3>
+    
 
         <div className="addItems">
          <input type="text" placeholder="📜 Add Item" className="form-control" value={inputData} 
           onChange={(event)=>setInputData(event.target.value)}/>
 
     
-         {toggleButton? (<i className="far fa-edit add-btn" onClick={addItem}></i>) : 
-         (<i className="fa fa-plus add-btn" onClick={addItem}></i>)
+         {toggleButton? (<i className="far fa-edit add-btn input-btn" onClick={addItem}></i>) : 
+         (<i className="fa fa-plus add-btn input-btn" onClick={addItem}></i>)
          }
           </div>
            
@@ -96,9 +104,9 @@ const Todo = () => {
                return( 
                  <>
                  <div className="eachItem" key={curElem.id}>
-              <h3>{curElem.name}</h3>
-              <div className="todo-btn">
-               <i className="far fa-edit add-btn" 
+                 <h3 className="tasks">{curElem.name}</h3>
+                <div className="todo-btn">
+                <i className="far fa-edit add-btn" 
                onClick={()=>editItem(curElem.id)}></i>
                <i className="far fa-trash-alt add-btn" onClick={()=>deleteItem(curElem.id)}></i>
               </div>
@@ -112,7 +120,7 @@ const Todo = () => {
           <div className="showItems">
            <button className="btn effect04" data-sm-link-text="Remove All" 
            onClick={removeAll}>
-            <span>CHECKLIST</span>
+            <span><strong>CHECKLIST</strong></span>
            </button>
           </div>
       </div>
